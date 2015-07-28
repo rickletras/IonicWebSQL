0e52ec1685ce7bb5baffb261155db5157259d5d5
Paulo Souza <paulo.souza@senior.com.br>
2015-07-28 11:25:00 -0300

    #1 exemplo de uso do websql com ionic

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,51 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    .controller('DashCtrl', function ($scope, webSQL) {
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+        var acessos = [{sentido_acesso: 'entrada', data: '23/02/2015', hora: '08:55', local_acesso: 'Portaria 1'}];
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+        webSQL.add(acessos).then(
+            function (res) {
+                console.log('Add executado com sucesso!',res);
+            }
+        );
+
+        webSQL.get("acessos").then(
+            function (res) {
+                console.log('GetAcessos executado com sucesso!',res);
+            }
+        );
+
+        webSQL.del().then(function (res) {
+            console.log('Del. executado com sucesso!', res);
+        });
+
+
+    })
+
+    .controller('ChatsCtrl', function ($scope, Chats) {
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
+
+        $scope.chats = Chats.all();
+        $scope.remove = function (chat) {
+            Chats.remove(chat);
+        };
+    })
+
+    .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+        $scope.chat = Chats.get($stateParams.chatId);
+    })
+
+    .controller('AccountCtrl', function ($scope) {
+        $scope.settings = {
+            enableFriends: true
+        };
+    });
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,149 @@
 angular.module('starter.services', [])
+    .factory('Database', function () {
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+        const
+            DB_NAME = "nomedabadse",
+            VERSION = "versao",
+            DESC = "descricao";
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
+        var createDatabase = function () {
+            return openDatabase(DB_NAME, VERSION, DESC, 2 * 1024 * 1024);
+        };
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+        var createTable = function () {
+            var db = createDatabase();
+            db.transaction(function (tx) {
+                tx.executeSql("CREATE TABLE IF NOT EXISTS tabela (coluna_1,coluna_2,coluna_3,coluna_4)", null,
+                    function (transaction, result) {
+                       console.log('Tabela criada com sucesso!');
+                    },
+                    function (transaction, error) {
+                        console.error(error.message);
+                    });
+            });
+        };
+
+        createDatabase();
+        createTable();
+        return {
+            open: createDatabase
         }
-      }
-      return null;
-    }
-  };
-});
+
+    })
+
+    .factory('webSQL', function (Database,$q) {
+
+
+        var openDataSql = function () {
+            return Database.open();
+        }
+
+        var insertData = function (value) {
+            var defer = $q.defer();
+            var db = openDataSql();
+
+            db.transaction(function (tx) {
+                value.forEach(function (element) {
+                    tx.executeSql("INSERT INTO tabela(coluna_1,coluna_2,coluna_3,coluna_4) VALUES (?,?,?,?)",
+                        [element.sentido_acesso,element.data,element.hora,element.local_acesso],
+                        function(transaction, result){
+                            defer.resolve(result);
+                        },
+                        function (transaction, error) {
+                            defer.reject(error);
+                        }
+                    );
+
+                });
+
+            });
+
+            return defer.promise;
+        }
+
+        var getAll = function () {
+            var db = openDataSql();
+            var defer = $q.defer();
+
+            db.transaction(function (tx) {
+                tx.executeSql("SELECT * FROM Tabela", null,
+                    function (transaction, result) {
+                        defer.resolve(result.rows);
+                    },
+                    function (transaction, error) {
+                        defer.reject(error);
+                    });
+            });
+            return defer.promise;
+        };
+
+        var deleteAll = function () {
+            var db = openDataSql();
+            var defer = $q.defer();
+            db.transaction(function (tx) {
+                tx.executeSql("DELETE FROM Tabela", null,
+                    function (transaction, result) {
+                        defer.resolve(result.rowsAffected);
+                    },
+                    function (transaction, error) {
+                        defer.$$reject(error);
+                    });
+            });
+            return defer.promise;
+        }
+        return {
+            add: insertData,
+            get: getAll,
+            del: deleteAll
+        }
+    })
+
+
+    .factory('Chats', function () {
+        // Might use a resource here that returns a JSON array
+
+        // Some fake testing data
+        var chats = [{
+            id: 0,
+            name: 'Ben Sparrow',
+            lastText: 'You on your way?',
+            face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+        }, {
+            id: 1,
+            name: 'Max Lynx',
+            lastText: 'Hey, it\'s me',
+            face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+        }, {
+            id: 2,
+            name: 'Adam Bradleyson',
+            lastText: 'I should buy a boat',
+            face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        }, {
+            id: 3,
+            name: 'Perry Governor',
+            lastText: 'Look at my mukluks!',
+            face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+        }, {
+            id: 4,
+            name: 'Mike Harrington',
+            lastText: 'This is wicked good ice cream.',
+            face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+        }];
+
+        return {
+            all: function () {
+                return chats;
+            },
+            remove: function (chat) {
+                chats.splice(chats.indexOf(chat), 1);
+            },
+            get: function (chatId) {
+                for (var i = 0; i < chats.length; i++) {
+                    if (chats[i].id === parseInt(chatId)) {
+                        return chats[i];
+                    }
+                }
+                return null;
+            }
+        };
+    });
